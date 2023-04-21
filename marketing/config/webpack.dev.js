c9c69123a7d0b7f17d3ee2ffff1c1a962025e7a6
Juanjo Jaramillo <juanjodev02@icloud.com>
2023-04-21 11:02:57 -0500

    feat: create container.yml workflow

--- marketing/config/webpack.dev.js
+++ marketing/config/webpack.dev.js
@@ -1,10 +1,6 @@
 const { merge } = require('webpack-merge');
 const commonConfig = require('./webpack.common.js');
-
-const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-
-const PackageJson = require('../package.json');
 
 const devConfig = {
     mode: 'development',
