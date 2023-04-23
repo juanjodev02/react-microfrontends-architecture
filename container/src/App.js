b6aea2ba2a5d27f45862446e10b26a488aaec3af
Juanjo Jaramillo <juanjodev02@icloud.com>
2023-04-22 20:57:04 -0500

    feat: fix css collisions issue

--- container/src/App.js
+++ container/src/App.js
@@ -1,17 +1,25 @@
 import React, {lazy, Suspense} from "react";
 import Header from "./components/Header";
 import {BrowserRouter} from "react-router-dom";
+import {createGenerateClassName, StylesProvider} from "@material-ui/core";
 const MarketingApp =  lazy(() => import("./components/MarketingApp"));
+
+
+const generateClassName = createGenerateClassName({
+    productionPrefix: 'co',
+});
 
 export default () => {
     return (
-        <BrowserRouter>
-            <>
-                <Header />
-                <Suspense fallback={<div>Loading...</div>}>
-                    <MarketingApp />
-                </Suspense>
-            </>
-        </BrowserRouter>
+        <StylesProvider generateClassName={generateClassName}>
+            <BrowserRouter>
+                <>
+                    <Header />
+                    <Suspense fallback={<div>Loading...</div>}>
+                        <MarketingApp />
+                    </Suspense>
+                </>
+            </BrowserRouter>
+        </StylesProvider>
     );
 };
