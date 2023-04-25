61746d381f8a8cef342670eb52fb7112ac3fae29
Juanjo Jaramillo <juanjodev02@icloud.com>
2023-04-25 11:59:13 -0500

    fix: error boundary component

--- container/src/components/Error.js
+++ container/src/components/Error.js
@@ -1,32 +1,10 @@
 import React from 'react';
 import PropTypes from "prop-types";
-import {makeStyles} from "@material-ui/core/styles";
-
-const useStyles = makeStyles((theme) => ({
-    errorContainer: {
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-        flexGrow: 1,
-        flex: 1,
-        height: '100%'
-    },
-    errorMessage: {
-        color: theme.palette.error.main
-    },
-    errorTitle: {
-        color: theme.palette.primary.main,
-        fontFamily: 'Roboto',
-    }
-}));
-
 const Error = ({ error }) => {
-    const classes = useStyles();
     return (
-        <section className={classes.errorContainer}>
-            <h1 className={classes.errorTitle}>Something were wrong</h1>
-            <div className={classes.errorMessage}>{error}</div>
+        <section>
+            <h1>Something were wrong</h1>
+            <div>{error}</div>
         </section>
     );
 }
