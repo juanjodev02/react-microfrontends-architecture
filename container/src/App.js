ca20e166ac862d1b9a67bbcacac7cfb8bd41a5be
Juanjo Jaramillo <juanjodev02@icloud.com>
2023-04-25 12:22:45 -0500

    fix: delete error boundary

--- container/src/App.js
+++ container/src/App.js
@@ -3,7 +3,6 @@ import Header from "./components/Header";
 import {BrowserRouter, Route, Switch} from "react-router-dom";
 import {createGenerateClassName, StylesProvider} from "@material-ui/core";
 import Progress from "./components/Progress";
-import ErrorBoundary from "./components/ErrorBoundary";
 
 const AuthApp = lazy(() => import('./components/AuthApp'));
 const MarketingApp = lazy(() => import('./components/MarketingApp'));
@@ -19,18 +18,16 @@ export default () => {
         <StylesProvider generateClassName={generateClassName}>
             <BrowserRouter>
                 <Header isSignedIn={isSignedIn} onSignOut={() => setIsSignedIn(false)} />
-                <ErrorBoundary>
-                    <Suspense fallback={<Progress />}>
-                        <Switch>
-                            <Route path="/auth">
-                                <AuthApp onSignIn={() => setIsSignedIn(true)} />
-                            </Route>
-                            <Route path="/">
-                                <MarketingApp />
-                            </Route>
-                        </Switch>
-                    </Suspense>
-                </ErrorBoundary>
+                <Suspense fallback={<Progress />}>
+                    <Switch>
+                        <Route path="/auth">
+                            <AuthApp onSignIn={() => setIsSignedIn(true)} />
+                        </Route>
+                        <Route path="/">
+                            <MarketingApp />
+                        </Route>
+                    </Switch>
+                </Suspense>
             </BrowserRouter>
         </StylesProvider>
     );
